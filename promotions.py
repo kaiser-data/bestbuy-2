2174083ddbbe8edfa1704ac5e0b87650a1e333b7
kaiser-data <61822885+kaiser-data@users.noreply.github.com>
2025-01-27 07:32:57 +0100

    new promotion class thirs one free

--- promotions.py
+++ promotions.py
@@ -26,3 +26,11 @@ class SecondHalfPrice(Promotion):
         full_price_items = quantity // 2 + quantity % 2
         half_price_items = quantity // 2
         return full_price_items * product.price + half_price_items * (product.price / 2)
+
+class ThirdOneFree(Promotion):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def apply_promotion(self, product, quantity):
+        chargeable_items = quantity - (quantity // 3)
+        return chargeable_items * product.price
